5820066fcc6f02ab496f326bb5c3ce5246a119fe
Wenhao Shao <whshawn@umich.edu>
2026-07-13 23:33:44 -0400

    roller

--- assets/js/render-news.js
+++ assets/js/render-news.js
@@ -48,8 +48,18 @@
     });
   }
 
+  function shuffle(arr) {
+    var a = arr.slice();
+    for (var i = a.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var tmp = a[i]; a[i] = a[j]; a[j] = tmp;
+    }
+    return a;
+  }
+
   function renderCarousel(mount, items) {
     if (items.length === 0) return;
+    items = shuffle(items);
 
     var dotsHtml = items.map(function (_, i) {
       return '<button class="news-card__dot' + (i === 0 ? ' active' : '') + '" data-index="' + i + '" aria-label="Show story ' + (i + 1) + '"></button>';
